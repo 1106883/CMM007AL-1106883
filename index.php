95ade8cbf1005ea339ea1bb8280b9bbca7ca4809
1106883 <1106883@rgu.ac.uk>
2016-05-04 10:46:57 +0100

    hi

--- index.php
+++ index.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1106883
- * Date: 04/05/2016
- * Time: 09:02
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="design.css"/>
+    <link rel="stylesheet"
+          href="unsemantic-grid-responsive-tablet.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"/>
+    <link href='https://fonts.googleapis.com/css?family=PT+Serif' rel='stylesheet' type='text/css'>
+    <meta charset="UTF-8">
+    <title>myBlog</title>
+</head>
+<body>
+
+<header>
+    <h1>myBlog</h1>
+    <p>because the internet needs to know what I think</p>
+</header>
+
+<nav>
+    <ul>
+        <a href="blog.php"><li>All Blog Items</li></a>
+        <a href="blog.php"><li>Work Items</li></a>
+        <a href="blog.php"><li>University Items</li></a>
+        <a href="blog.php"><li>Family Items</li></a>
+        <a href="add.php"><li>Insert a Blog Item</li></a>
+    </ul>
+</nav>
+
+<div id="content">
+    <section class="grid-25">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+    </section>
+    <section class="grid-25">
+        <img src="blog.png">
+    </section>
+</div>
+
+
+<section class="grid-100">
+    <footer>
+        Designed by Kyle Martin, 2016
+    </footer>
+</section>
+
+</body>
+
+</html>
